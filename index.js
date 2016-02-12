1f4a1e24a763f70974c3b5d33a19da65072bcd76
John Winstead <john@jwin.io>
2016-02-12 12:50:31 -0800

    implement msgQueue

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var MsgQueuePlugin = require('msgqueue-server');
+var msgQueue = new MsgQueuePlugin(app);
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
